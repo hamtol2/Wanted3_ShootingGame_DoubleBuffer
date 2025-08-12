c9165e61e6da718cc641e6f877a2fa1a3921d7c4
Ronnie Jang <ronniej@naver.com>
2025-08-13 00:11:51 +0900

    Color 색상 값 추가

--- Engine/Math/Color.h
+++ Engine/Math/Color.h
@@ -5,7 +5,13 @@ enum class Color : int
 {
 	Blue = 1,
 	Green = 2,
+	Cyan = Blue | Green,
 	Red = 4,
+	Magenta = Red | Blue,
 	White = Red | Green | Blue,
 	Intensity = 8,
+	BackgroundBlue = 16,
+	BackgroundGreen = 32,
+	BackgroundRed = 64,
+	BackgroundIntensity = 128,
 };
